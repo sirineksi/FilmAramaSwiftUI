a3dcbb3958da1e9c0c83a914c7e4ca6d8cea78af
Şirin Ekşi <sirin.eksi@hotmail.com>
2023-05-29 11:39:48 +0300

    Initial Commit

---
+++ FilmAramaSwiftUI/FilmAramaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmAramaSwiftUIApp.swift
+//  FilmAramaSwiftUI
+//
+//  Created by ceksi on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FilmAramaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
